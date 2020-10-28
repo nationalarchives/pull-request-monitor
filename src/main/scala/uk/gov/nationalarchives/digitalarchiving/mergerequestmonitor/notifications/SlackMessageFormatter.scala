30c6f7c698786788d0d1be8555d2b5a5b1d8cab5
sam <sam.palmer@nationalarchives.gov.uk>
2020-10-28 10:27:22 +0000

    Change the formatter to print stale messages

--- src/main/scala/uk/gov/nationalarchives/digitalarchiving/mergerequestmonitor/notifications/SlackMessageFormatter.scala
+++ src/main/scala/uk/gov/nationalarchives/digitalarchiving/mergerequestmonitor/notifications/SlackMessageFormatter.scala
@@ -3,25 +3,42 @@ package uk.gov.nationalarchives.digitalarchiving.mergerequestmonitor.notificatio
 import uk.gov.nationalarchives.digitalarchiving.mergerequestmonitor.notifications.presenters.{MergeRequestSlackPresenter, ProjectSlackPresenter}
 
 object SlackMessageFormatter {
+
   def format(projects: Seq[ProjectSlackPresenter], mergeRequestsTerm: MergeRequestsTerm): String = {
+    case class PullRequests(name: String, pullRequests: Seq[String])
     val projectsWithOpenMrs = projects.filter(project => project.mergeRequests.nonEmpty)
 
     if (projectsWithOpenMrs.isEmpty) {
       s"No open ${mergeRequestsTerm.term} to review! :tada:"
     } else {
-      val projectMessages = projectsWithOpenMrs.map(project => {
-        val mergeRequestMessages = project.mergeRequests.map(mergeRequest => formatMergeRequest(mergeRequest, project)).mkString("\n")
-        s"*${project.name}*\n$mergeRequestMessages"
-      }).mkString("\n")
+      def projectsToString(filterFn: MergeRequestSlackPresenter => Boolean, strFn: MergeRequestSlackPresenter => String) = {
+        projectsWithOpenMrs.map(pr => {
+          val prs = pr.mergeRequests.filter(filterFn).map(strFn)
+          if (prs.nonEmpty) {
+            s"*${pr.name}*\n${prs.mkString("\n")}"
+          } else {
+            ""
+          }
+        }).filter(_.nonEmpty).mkString("\n")
+      }
+      def newPrToString: MergeRequestSlackPresenter => String = mergeRequest => s"     ${mergeRequest.authorName}: <${mergeRequest.url}|${mergeRequest.title}>"
+      val newPrProjectMessages: String = projectsToString(pr => pr.daysSinceLastUpdate < 2, newPrToString)
+      val oldPrProjectMessages: String = projectsToString(pr => pr.daysSinceLastUpdate >= 2, mergeRequest => s"${newPrToString(mergeRequest)} ${updatedSince(mergeRequest.daysSinceLastUpdate)}")
 
-      s"Hello team!\nHere are the ${mergeRequestsTerm.term} to review today:\n${projectMessages}"
+      val output = "Hello team!\n"
+      val newPrs = if (!newPrProjectMessages.isEmpty) output ++ "Here are the pull requests to review today:\n" ++ newPrProjectMessages ++ "\n" else output
+      if (!oldPrProjectMessages.isEmpty) newPrs ++ "These pull requests have had no activity for two days:\n" ++ oldPrProjectMessages else newPrs
     }
   }
 
-  private def formatMergeRequest(mergeRequest: MergeRequestSlackPresenter, project: ProjectSlackPresenter): String = {
-    s"${mergeRequest.authorName}: <${mergeRequest.url}|${mergeRequest.title}>"
-  }
+  private def updatedSince(days: Long): String = s"Updated $days ${if (days == 1) "day" else "days"} ago"
+
 }
 
-sealed trait MergeRequestsTerm { def term: String }
-case object PULL_REQUEST extends MergeRequestsTerm { val term = "pull requests" }
+sealed trait MergeRequestsTerm {
+  def term: String
+}
+
+case object PULL_REQUEST extends MergeRequestsTerm {
+  val term = "pull requests"
+}
